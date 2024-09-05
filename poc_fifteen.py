0c290c99790db7b6ddbfa38811356b3eee01c383
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2024-09-05 10:05:59 +0100

    poc_fifteen.py

--- poc_fifteen.py
+++ poc_fifteen.py
@@ -3,14 +3,12 @@ Loyd's Fifteen puzzle - solver and visualizer
 Note that solved configuration has the blank (zero) tile in upper left
 Use the arrows key to swap this tile with its neighbors
 """
-
-# import poc_fifteen_gui
+import poc_fifteen_gui
 
 class Puzzle:
     """
     Class representation for the Fifteen puzzle
     """
-
     def __init__(self, puzzle_height, puzzle_width, initial_grid = None):
         """
         Initialize puzzle with default height and width
@@ -32,10 +30,10 @@ class Puzzle:
         Generate string representaion for puzzle
         Returns a string
         """
-        ans = ""
+        ans = ''
         for row in range(self._height):
             ans += str(self._grid[row])
-            ans += "\n"
+            ans += '\n'
         return ans
 
     #####################################
@@ -91,7 +89,7 @@ class Puzzle:
             for col in range(self._width):
                 if self._grid[row][col] == solved_value:
                     return (row, col)
-        assert False, "Value " + str(solved_value) + " not found"
+        assert False, 'Value ' + str(solved_value) + ' not found'
 
     def update_puzzle(self, move_string):
         """
@@ -99,28 +97,28 @@ class Puzzle:
         """
         zero_row, zero_col = self.current_position(0, 0)
         for direction in move_string:
-            if direction == "l":
-                assert zero_col > 0, "move off grid: " + direction
+            if direction == 'l':
+                assert zero_col > 0, 'move off grid: ' + direction
                 self._grid[zero_row][zero_col] = self._grid[zero_row][zero_col - 1]
                 self._grid[zero_row][zero_col - 1] = 0
                 zero_col -= 1
-            elif direction == "r":
-                assert zero_col < self._width - 1, "move off grid: " + direction
+            elif direction == 'r':
+                assert zero_col < self._width - 1, 'move off grid: ' + direction
                 self._grid[zero_row][zero_col] = self._grid[zero_row][zero_col + 1]
                 self._grid[zero_row][zero_col + 1] = 0
                 zero_col += 1
-            elif direction == "u":
-                assert zero_row > 0, "move off grid: " + direction
+            elif direction == 'u':
+                assert zero_row > 0, 'move off grid: ' + direction
                 self._grid[zero_row][zero_col] = self._grid[zero_row - 1][zero_col]
                 self._grid[zero_row - 1][zero_col] = 0
                 zero_row -= 1
-            elif direction == "d":
-                assert zero_row < self._height - 1, "move off grid: " + direction
+            elif direction == 'd':
+                assert zero_row < self._height - 1, 'move off grid: ' + direction
                 self._grid[zero_row][zero_col] = self._grid[zero_row + 1][zero_col]
                 self._grid[zero_row + 1][zero_col] = 0
                 zero_row += 1
             else:
-                assert False, "invalid direction: " + direction
+                assert False, 'invalid direction: ' + direction
 
     ##################################################################
     # Phase one methods
@@ -133,7 +131,7 @@ class Puzzle:
         """
         assert target_row > 1 or target_row == 1 and \
             target_col == self.get_width() - 1, \
-            "i <= 1 and (i != 1 or j != n - 1)"
+            'i <= 1 and (i != 1 or j != n - 1)'
 
         width = self.get_width()
         if self.get_number(target_row, target_col) != 0:
@@ -150,11 +148,11 @@ class Puzzle:
         Place correct tile at target position
         Updates puzzle and returns a move string
         """
-        assert target_col > 0, "j <= 0"
+        assert target_col > 0, 'j <= 0'
         assert self.lower_row_invariant(target_row, target_col), \
-            "lower_row_invariant(i, j) is False"
+            'lower_row_invariant(i, j) is False'
 
-        move_string = ""
+        move_string = ''
         zero_row, zero_col = self.current_position(target_row, target_col)
         move_string += self.move_zero_to_position(target_row, target_col,
             zero_row, zero_col)
@@ -166,7 +164,7 @@ class Puzzle:
             target_row, target_col - 1)
 
         assert self.lower_row_invariant(target_row, target_col - 1), \
-            "lower_row_invariant(i, j - 1) is False"
+            'lower_row_invariant(i, j - 1) is False'
         return move_string
 
     def solve_col0_tile(self, target_row):
@@ -175,7 +173,7 @@ class Puzzle:
         Updates puzzle and returns a move string
         """
         assert self.lower_row_invariant(target_row, 0), \
-            "lower_row_invariant(i, 0) is False"
+            'lower_row_invariant(i, 0) is False'
 
         zero_row, zero_col = target_row - 1, 1
         move_string = self.move_zero_to_position(target_row, 0,
@@ -192,18 +190,18 @@ class Puzzle:
 
             zero_row, zero_col = self.current_position(0, 0) # TODO
             if zero_row == target_row - 1 and zero_col != 0:
-                move_string += "u"
-                self.update_puzzle("u")
+                move_string += 'u'
+                self.update_puzzle('u')
             move_string += self.move_zero_to_position(zero_row, zero_col,
                 target_row - 1, 0)
-            move_string += "ruldrdlurdluurddlur"
-            self.update_puzzle("ruldrdlurdluurddlur")
+            move_string += 'ruldrdlurdluurddlur'
+            self.update_puzzle('ruldrdlurdluurddlur')
 
         move_string += self.move_zero_to_position(target_row - 1, 1,
             target_row - 1, self.get_width() - 1)
 
         assert self.lower_row_invariant(target_row - 1, self.get_width() - 1), \
-            "lower_row_invariant(i - 1, n - 1) is False"
+            'lower_row_invariant(i - 1, n - 1) is False'
         return move_string
 
     def move_zero_to_position(self, zero_row, zero_col, row, col):
@@ -211,20 +209,20 @@ class Puzzle:
         Place zero tile at position (row, col)
         Updates puzzle and returns a move string
         """
-        assert self.get_number(zero_row, zero_col) == 0, "number(i, j) != 0"
+        assert self.get_number(zero_row, zero_col) == 0, 'number(i, j) != 0'
 
-        move_string = ""
+        move_string = ''
         if row < zero_row:
-            move_string += "u" * (zero_row - row)
+            move_string += 'u' * (zero_row - row)
         if col < zero_col:
-            move_string += "l" * (zero_col - col)
+            move_string += 'l' * (zero_col - col)
         elif zero_col < col :
-            move_string += "r" * (col - zero_col)
+            move_string += 'r' * (col - zero_col)
         if zero_row < row:
-            move_string += "d" * (row - zero_row)
+            move_string += 'd' * (row - zero_row)
         self.update_puzzle(move_string)
 
-        assert self.get_number(row, col) == 0, "number(i, j) != 0"
+        assert self.get_number(row, col) == 0, 'number(i, j) != 0'
         return move_string
 
     def position_tile(self, zero_row, zero_col, position,
@@ -234,38 +232,38 @@ class Puzzle:
         Updates puzzle and returns a move string
         """
         assert self.get_number(zero_row, zero_col) == 0, \
-            "number(zero_row, zero_col) != 0"
+            'number(zero_row, zero_col) != 0'
 
         move_string = ""
         row, col = position
         while (row, col) != (target_row, target_col):
             assert zero_row <= row and row <= target_row, \
-                "zero_row > row or row > target_row"
+                'zero_row > row or row > target_row'
             assert abs(row - zero_row) + abs(col - zero_col) == 1, \
-                "abs(row - zero_row) + abs(col - zero_col) != 1"
+                'abs(row - zero_row) + abs(col - zero_col) != 1'
 
             if col != target_col:
                 diff = target_col - col
                 if col > target_col:
-                    move_string += ("ulldr" if row > 0 else "dllur") * abs(diff)
+                    move_string += ('ulldr' if row > 0 else 'dllur') * abs(diff)
                 else: # target_col > col
-                    move_string += ("urrdl" if row > 0 else "drrul") * abs(diff)
+                    move_string += ('urrdl' if row > 0 else 'drrul') * abs(diff)
                 col += diff
                 zero_col += diff
             else: # row != target_row
                 if col != zero_col:
                     if row > 0:
-                        move_string += "ur" if col > zero_col else "ul"
+                        move_string += 'ur' if col > zero_col else 'ul'
                         zero_row -= 1
                     else:
-                        move_string += "dru" if col > zero_col else "dlu"
+                        move_string += 'dru' if col > zero_col else 'dlu'
                         row += 1
                     if col > zero_col:
                         zero_col += 1
                     else: # zero_col > col
                         zero_col -= 1
                 diff = target_row - row
-                move_string += ("lddru" if col > 0 else "rddlu") * diff
+                move_string += ('lddru' if col > 0 else 'rddlu') * diff
                 row += diff
                 zero_row += diff
 
@@ -276,13 +274,13 @@ class Puzzle:
         """
         Return previous position of zero title
         """
-        if move_string[-1] == "l":
+        if move_string[-1] == 'l':
             return (zero_row, zero_col + 1)
-        elif move_string[-1] == "r":
+        elif move_string[-1] == 'r':
             return (zero_row, zero_col - 1)
-        elif move_string[-1] == "u":
+        elif move_string[-1] == 'u':
             return (zero_row + 1, zero_col)
-        elif move_string[-1] == "d":
+        elif move_string[-1] == 'd':
             return (zero_row - 1, zero_col)
 
     #############################################################
@@ -326,8 +324,8 @@ class Puzzle:
         Solve the tile in row zero at the specified column
         Updates puzzle and returns a move string
         """
-        assert target_col > 1, "j <= 1"
-        assert self.row0_invariant(target_col), "row0_invariant(j) is False"
+        assert target_col > 1, 'j <= 1'
+        assert self.row0_invariant(target_col), 'row0_invariant(j) is False'
 
         zero_row, zero_col = 1, target_col - 1
         move_string = self.move_zero_to_position(0, target_col,
@@ -345,11 +343,11 @@ class Puzzle:
             zero_row, zero_col = self.current_position(0, 0) # TODO
             move_string += self.move_zero_to_position(zero_row, zero_col,
                 1, target_col - 2)
-            move_string += "urdlurrdluldrruld"
-            self.update_puzzle("urdlurrdluldrruld")
+            move_string += 'urdlurrdluldrruld'
+            self.update_puzzle('urdlurrdluldrruld')
 
         assert self.row1_invariant(target_col - 1), \
-            "row1_invariant(j - 1) is False"
+            'row1_invariant(j - 1) is False'
         return move_string
 
     def solve_row1_tile(self, target_col):
@@ -357,10 +355,10 @@ class Puzzle:
         Solve the tile in row one at the specified column
         Updates puzzle and returns a move string
         """
-        assert target_col > 1, "j <= 1"
-        assert self.row1_invariant(target_col), "row1_invariant(j) is False"
+        assert target_col > 1, 'j <= 1'
+        assert self.row1_invariant(target_col), 'row1_invariant(j) is False'
 
-        move_string = ""
+        move_string = ''
         zero_row, zero_col = self.current_position(1, target_col)
         move_string += self.move_zero_to_position(1, target_col,
             zero_row, zero_col)
@@ -371,7 +369,7 @@ class Puzzle:
         move_string += self.move_zero_to_position(zero_row, zero_col,
             0, target_col)
 
-        assert self.row0_invariant(target_col), "row0_invariant(j) is False"
+        assert self.row0_invariant(target_col), 'row0_invariant(j) is False'
         return move_string
 
     ###########################################################
@@ -390,10 +388,10 @@ class Puzzle:
                 self.get_number(1, 0) == width and \
                 self.get_number(1, 1) == width + 1:
                 return move_string
-            move_string += "rdlu"
-            self.update_puzzle("rdlu")
+            move_string += 'rdlu'
+            self.update_puzzle('rdlu')
 
-        assert False, "permutation is odd"
+        assert False, 'permutation is odd'
 
     def solve_puzzle(self):
         """
@@ -417,4 +415,4 @@ class Puzzle:
         return move_string
 
 # Start interactive simulation
-# poc_fifteen_gui.FifteenGUI(Puzzle(4, 4))
+poc_fifteen_gui.FifteenGUI(Puzzle(4, 4))
